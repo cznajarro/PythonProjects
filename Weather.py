04293b3f4bc2f824d2f2a9c84f93780778e02a17
Carlos Zuniga <carlos.zuniganajaarro@outlook.com>
2026-02-04 18:19:05 -0800

    Integrated Flask. Starting on the front-end.

--- Weather.py
+++ Weather.py
@@ -1,4 +1,7 @@
+from flask import Flask
 import requests
+
+app = Flask(__name__)
 
 api_key = 'f4162f1bcf5a36a7ab1dd7e225196f88'
 
@@ -6,10 +9,18 @@ user_input = input("Enter city: ")
 
 weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
 
+weather_statement = "Idk, look outside."
+
+@app.route("/")
+def index():
+   return weather_statement
+
 if weather_data.json()['cod'] == '404':
     print("Error, try again")
 else:
     weather = weather_data.json()['weather'][0]['main']
     temp = round(weather_data.json()['main']['temp'])
 
-    print(f"The weather in {user_input} is {weather}. \nThe temperature in {user_input} is {temp} degrees Fahrenheit.")
+    weather_statement = f"The weather in {user_input} is {weather}. \nThe temperature in {user_input} is {temp} degrees Fahrenheit."
+
+app.run(host="0.0.0.0", port=80)
